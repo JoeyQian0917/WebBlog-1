b3df0309b05cf0fd2142edfb0f7ddb7c35d91a66
mayunyi <952359947@qq.com>
2017-10-25 17:57:09 +0800

    js特效

--- app/components/hello.js
+++ app/components/hello.js
@@ -1,12 +1,12 @@
-import React  from 'react';
+import React, { Component } from 'react';
 import './hello.less';
 
 
-export default class Hello extends React.Component {
+export default class Hello extends Component {
 	render(){
 		return (
 			<div className="hello-component">
-				妈妈说我是最帅的小伙子！！！！！！我也是这么认为的 ！我是最帅的！！
+				妈妈说我是最帅的小伙子！！！！！！我也是这么认为的 
 			</div>
 		);
 	}
